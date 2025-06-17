003a4b8539852d0f4f7f70a0221746fe685ee287
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2025-06-17 15:42:04 -0300

    fix: ajustes e melhorias

--- src/test/java/modulos/utils/DriverConfig.java
+++ src/test/java/modulos/utils/DriverConfig.java
@@ -1,5 +1,6 @@
 package modulos.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,6 +10,7 @@ public class DriverConfig {
 
     public static void configurarDriverDoNavegadorWeb() {
       //  System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe"); //webdriver.edge.driver e msedgedriver.exe
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(); //new EdgeDriver()
         driver.manage().window().maximize();
     }
