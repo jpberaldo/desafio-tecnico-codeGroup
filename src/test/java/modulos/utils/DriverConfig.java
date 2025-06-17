131fe78ce01291b6d0afc71f963a5925a4213568
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2025-06-17 16:00:49 -0300

    fix: ajustando pipe

--- src/test/java/modulos/utils/DriverConfig.java
+++ src/test/java/modulos/utils/DriverConfig.java
@@ -3,6 +3,7 @@ package modulos.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverConfig {
 
@@ -11,7 +12,17 @@ public class DriverConfig {
     public static void configurarDriverDoNavegadorWeb() {
       //  System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe"); //webdriver.edge.driver e msedgedriver.exe
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(); //new EdgeDriver()
+        String ambiente = System.getProperty("ambiente", "local");
+
+        if (ambiente.equals("ci")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless", "--disable-gpu", "--no-sandbox");
+            driver = new ChromeDriver(options);
+        } else {
+            System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+
         driver.manage().window().maximize();
     }
 
